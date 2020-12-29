15741f9d550dbc98adc0df65e68237787141eb3a
mohamed atef <59102121+mohamedatef73@users.noreply.github.com>
2020-12-29 18:00:36 +0200

    data table functionality

--- src/DataTable.js
+++ src/DataTable.js
@@ -2,67 +2,101 @@ import React, { useState, useRef, useEffect } from 'react'
 import './App.css';
 
 
+
 export default function DataTable() {
     const [rows, setRows] = useState(3)
     const [columns, setColumns] = useState(3)
     const activeCell = useRef(null)
 
-    function handelNavigation(event) {
+
+    useEffect(() => {
+        const firstCell = document.getElementById("firstCell")
+        if (firstCell) {
+            activeCell.current = firstCell
+            activeCell.current.focus()
+            activeCell.current.style.backgroundColor = 'pink';
+
+        }
+
+    }, [columns, rows])
+
+
+
+    /**
+     * to handle navigation using keyboard arrows
+     */
+    useEffect(() => {
+        document.onkeydown = checkKey
+    })
+
+
+    function checkKey(event) {
         const { keyCode } = event;
 
-        if (keyCode === 40) { //down
-
-            activeCell.current.focus()
-            if (activeCell.current && activeCell.current.previousSibling) {
-                activeCell.current.previousSibling.focus();
-            }
-        }
-        if (keyCode === 38) { // up
-
-            activeCell.current.focus()
-            if (activeCell.current && activeCell.current.nextSibling) {
-                activeCell.current.nextSibling.focus();
-            }
-
+        if (keyCode === 40) { //down key
+            downKeyPressed()
         }
 
-        if (keyCode === 37) { // left
-
-            activeCell.current.focus()
-            if (activeCell.current && activeCell.current.previousSibling) {
-                activeCell.current.previousSibling.focus();
-            }
-
+        if (keyCode === 38) { // up key
+            upKeyPressed()
         }
 
-        if (keyCode === 39) { //right
+        if (keyCode === 37) { // left key
+            leftKeyPressed()
+        }
 
-            activeCell.current.focus()
-            if (activeCell.current && activeCell.current.nextSibling) {
-                activeCell.current.nextSibling.focus();
-            }
-
+        if (keyCode === 39) { //right key
+            rightKeyPressed()
         }
     }
 
 
-    function focusCell() {
-        activeCell.current.focus()
-    }
-
-    function gotToThePraviousCell() {
-        activeCell.current.focus()
-        if (activeCell.current && activeCell.current.previousSibling) {
-            activeCell.current.previousSibling.focus();
+    function downKeyPressed() {
+        if (activeCell.current) {
+            let index = activeCell.current.cellIndex;
+            let nextrow = activeCell.current.parentElement.nextElementSibling;
+            if (nextrow != null) {
+                let sibling = nextrow.cells[index];
+                goToTarget(sibling);
+            }
         }
 
     }
 
+    function upKeyPressed() {
+        if (activeCell.current) {
+            let index = activeCell.current.cellIndex;
+            let nextrow = activeCell.current.parentElement.previousElementSibling;
+            if (nextrow != null) {
+                let sibling = nextrow.cells[index];
+                goToTarget(sibling);
+            }
+        }
 
-    function gotToTheNextCell() {
-     activeCell.current.focus()
-        if (activeCell.current && activeCell.current.nextSibling) {
-            activeCell.current.nextSibling.focus();
+    }
+
+    function leftKeyPressed() {
+        if (activeCell.current) {
+            let sibling = activeCell.current.previousElementSibling;
+            goToTarget(sibling);
+        }
+    }
+
+    function rightKeyPressed() {
+        if (activeCell.current) {
+            let sibling = activeCell.current.nextElementSibling;
+            goToTarget(sibling);
+        }
+    }
+
+
+    function goToTarget(sibling) {
+        if (sibling != null) {
+            activeCell.current.focus();
+            activeCell.current.style.backgroundColor = '';
+            sibling.focus();
+            sibling.style.backgroundColor = 'pink';
+            activeCell.current = sibling;
         }
     }
 
@@ -79,25 +113,24 @@ export default function DataTable() {
                 <div className='col-12 col-md-6'>
 
                     <span>Number Of Columns</span> :
-
                         <input type='number' value={columns}
                         onChange={e => setColumns(e.target.value)} />
                 </div>
                 <div className="col-12">
-                    <button >Up</button>
-                    <button>Down</button>
+                    <button onClick={upKeyPressed}>Up</button>
+                    <button onClick={downKeyPressed}>Down</button>
 
-                    <button onClick={gotToThePraviousCell}>Left</button>
-                    <button onClick={gotToTheNextCell}>Right</button>
+                    <button onClick={leftKeyPressed}>Left</button>
+                    <button onClick={rightKeyPressed}>Right</button>
                 </div>
             </div>
             <div className='row my-5'>
                 <table>
                     <tbody>
                         {
-                            Array.from({ length: rows }, (val, i) => i + 1).map((row) => <tr key={row} ref={activeCell}>
+                            Array.from({ length: rows }, (val, i) => i + 1).map((row) => <tr key={row} >
                                 {Array.from({ length: columns }, (val, i) => i + 1).map((col) =>
-                                    <td onKeyDown={handelNavigation} tabIndex="1" onClick={focusCell}  key={row+col} >
+                                    <td ref={activeCell} id={row === 1 && col === 1 ? 'firstCell' : ''} key={row + col} >
                                         {row + col}</td>)}
                             </tr>)
                         }
